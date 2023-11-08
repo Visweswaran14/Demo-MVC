183aac055538d590dcf52988ff56326405e6ca78
Visweswaran14 <visweswaran142001@gmail.com>
2023-11-08 15:53:34 +0530

    initial commit

---
+++ Models/Class2Context.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Dotnet2.Models
+{
+    public partial class Class2Context : DbContext
+    {
+        public Class2Context()
+        {
+        }
+
+        public Class2Context(DbContextOptions<Class2Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=PF3XGBCR230817\\SQLEXPRESS;Database=Class2;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("employee");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dotnet2.Models
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "UserName is required")]
+        [MinLength(4)]
+        [MaxLength(20)]
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+    }
+}
